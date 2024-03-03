e4f813ee409cc9d28ff77082794623697f67f8c2
arano1212 <arano1212@gmail.com>
2024-03-03 02:34:47 -0600

    testing terminado

--- cypress/e2e/project.cy.js
+++ cypress/e2e/project.cy.js
@@ -1,15 +1,25 @@
-/* describe('funcionalidad de login', () => {
+ describe('funcionalidad de login', () => {
   it('mi aplicacion cargue login', () => {
     cy.visit('/login')
     cy.get('h1').contains('Please sign in')
   })
 
   it('Probar el Login', () => {
-    cc
-  })
-}) */
+    cy.intercept('POST', 'https://ecommerce-json-jwt.onrender.com/login').as('loginRequest')
+    cy.visit('/login')
+    cy.get('input[name="email"]').type('drstrange@marvel.com')
+    cy.get('input[name="password"]').type('multiverso')
+    cy.get('button[type="submit"]').click()
 
-/* describe('Que mi app cargue HOME despues de Login', () => {
+    cy.wait('@loginRequest').its('response.statusCode').should('eq', 200)
+    cy.get('h1').contains('Pokédex').should('be.visible')
+    cy.url().should('include', '/')
+    
+    
+  })
+}) 
+
+ describe('Que mi app cargue HOME despues de Login', () => {
   it(' Carque la PokeApi', () => {
     cy.intercept('GET', 'https://pokeapi.co/api/v2/pokemon?limit=151').as('apiPokedex')
     cy.visit('/login')
@@ -21,9 +31,9 @@
     cy.get('h1').contains('Pokédex').should('be.visible')
     cy.url().should('include', '/')
   })
-}) */
+}) 
 
-/* describe('Que mi app cargue register', () => {
+describe('Que mi app cargue register', () => {
   it('Probar el register', () => {
     cy.intercept('POST', 'https://ecommerce-json-jwt.onrender.com/register').as('registerRequest')
     cy.visit('/signup')
@@ -33,16 +43,16 @@
 
     cy.get('input[name="first_name"]').type('a')
     cy.get('input[name="last_name"]').type('a')
-    cy.get('select[name="gender"]').select('F')
+    cy.get('select[name="gender"]').select('Male')
     cy.get('input[name="email"]').type(dynamicEmail)
     cy.get('input[name="password"]').type(dynamicPassword)
     cy.get('button[type="submit"]').click()
 
-    cy.wait('@registerRequest').its('response.statusCode').should('eq', 200)
+    cy.wait('@registerRequest').its('response.statusCode').should('eq', 201)
     cy.url().should('include', '/login')
     cy.get('h1').contains('Please sign in')
   })
-}) */
+}) 
 
 describe('logout', () => {
   it('salir con logout', () => {
@@ -55,8 +65,10 @@ describe('logout', () => {
 
     cy.url().should('include', '/')
     cy.contains('Home')
-    cy.get('Nav > Nav.Link').eq(1).click()
+    cy.get('.navbar-toggler').click()
+    cy.contains('a', 'Logout').click()
+
     cy.url().should('include', '/login')
-    cy.contains('Please sign in')
+    cy.get('h1').contains('Please sign in')
   })
 })
